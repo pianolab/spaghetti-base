0260b54912da59574a86862b45ce442f4aacb315
Djalma Araújo <djalma.araujo@gmail.com>
2010-11-18 15:59:47 -0300

    Add Lang Controller as a Default Controller to multilang applications

---
+++ app/controllers/lang_controller.php
@@ -0,0 +1,11 @@
+<?php
+class LangController extends AppController
+{
+	public $uses = array();
+	public function change($lang)
+	{
+		$this->LangComponent->setLang($lang);
+		$this->redirect('/');
+	}
+}
+?>
