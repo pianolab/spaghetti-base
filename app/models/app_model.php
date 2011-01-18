e1503ff741e4ea96a2223e567bdd4520b2433b7c
Djalma Araújo <djalma.araujo@gmail.com>
2011-01-18 16:25:47 -0300

    Add a hasEqualInformations methods for check database duplicated informations

--- app/models/app_model.php
+++ app/models/app_model.php
@@ -10,7 +10,18 @@
  */
     
 class AppModel extends Model {
-    
+	/**
+	 * Check for equal information in database based on cantBeEqualFields public variable of each model
+	 */ 
+	public function hasEqualInformations($data, $id = null)
+	{
+		$return = false;
+		foreach ($this->cantBeEqualFields as $field):
+			$conditions = ($id) ? array('id <>' => $id, "$field" => $data["$field"]) : array("$field" => $data["$field"]);
+			if ($find_field = $this->first(array('conditions' => $conditions, 'recursion' => -1))):
+				$return["$field"] = 'Já existe informações no banco de dados com os mesmos caracteres';
+			endif;
+		endforeach;
+		return $return;
+	}
 }
-    
-?>
