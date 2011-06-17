e56a2984eb2e8bdc2574556ce732e41ee1a01862
Djalma Araújo <djalma.araujo@gmail.com>
2011-06-17 09:40:08 -0300

    Added analytics element

--- app/views/shared/_header.htm.php
+++ app/views/shared/_header.htm.php
@@ -6,6 +6,7 @@
 	<meta name="author" content="Pianolab">
 	<?php echo $html->stylesheet(array("screen.css")); ?>
 	<?php echo $html->script(array("jquery-1.6.1.min.js", "application.js")); ?>
+	<?php echo $this->element('shared/analytics'); ?>
 </head>
 <body>
 	<div id="container">
