b22923d3881755ea817144eb1afdd0f07ce80f8a
Djalma Araújo de Andrade <djalma.araujo@gmail.com>
2012-03-16 15:19:11 -0300

    Instructions

--- app/views/home/index.htm.php
+++ app/views/home/index.htm.php
@@ -7,11 +7,23 @@
 
 <!-- Example row of columns -->
 <div class="row">
-  <div class="span12">
-		<h2>Instructions</h2>
-		<pre>
+	<div class="span6">
+	<h2>Instructions</h2>
+	<pre>
 1. git clone https://pianolab@github.com/pianolab/spaghetti-base.git
 2. remove .git folder
 3. work!</pre>
-  </div>
+	</div>
+
+	<div class="span6">
+		<h2>Contains</h2>
+		<ul>
+			<li>Full MVC Structure (Spaghetti Framework)</li>
+			<li>Full bootstrap + JS plugins + GRID system (Bootstrap, from Twitter)</li>
+			<li>E-mail Component (Swift Mailer)</li>
+			<li>Text, Date, Currency, Youtube.. Helpers</li>
+			<li>Lot of snippets (Facebook tags, Analytics, mobile favicons, pagination example)</li>
+		</ul>
+	</div>
 </div>
+<hr />
