11671503b0badba82074036cafefb21c90a0ee4a
Djalma Araújo de Andrade <djalma.araujo@gmail.com>
2012-03-16 15:29:47 -0300

    Fix indent home controller

--- app/controllers/home_controller.php
+++ app/controllers/home_controller.php
@@ -1,7 +1,7 @@
 <?php
 class HomeController extends AppController {
-    public $uses = array();
-    public function index() {}
+	public $uses = array();
+	public function index() {
+		
+	}
 }
-
-?>
