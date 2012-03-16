523966513c9a1248819ebc513a0dc62897ae581a
Djalma Araújo de Andrade <djalma.araujo@gmail.com>
2012-03-16 16:09:30 -0300

    new error layout

--- app/layouts/error.htm.php
+++ app/layouts/error.htm.php
@@ -1,25 +1,23 @@
-<!DOCTYPE HTML>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width,initial-scale=1">
-	<title>Erro encontrado!</title>
-	<meta name="author" content="Pianolab">
-	<?php echo $html->stylesheet(array("sample.css")); ?>
-	<?php echo $this->element('shared/analytics'); ?>
-</head>
-<body>
-	<section id="container">
-		<header></header>
-		<div id="content">
-			<div id="wrapper">
-			    <h1>Error.page.404</h1>
-			    <?php if (Config::read('debug')): ?>
-					<?php echo $this->contentForLayout; ?>
-				<?php endif; ?>
-		  </div>
-		</div>
-	</section> <!-- End Container -->
-	<footer></footer>
-</body>
-</html>
+<?php echo $this->element('shared/header', $this->data); ?>
+			<div class="hero-unit">
+				<h1>Error.page.404</h1>
+				<?php echo $this->contentForLayout; ?>
+				<p><a href="<?php echo Mapper::url('/', true); ?>" class="btn btn-inverse btn-large">Go to HOME</a></p>
+			</div>
+
+	    </div> <!-- /container -->
+		<footer class="row">
+			<div class="span12 center">
+				<p>piano.base <a href="http://www.pianolab.com.br/">@pianolabweb</a>. It's released under the MIT license.</p>
+			</div>
+		</footer>
+
+	    <!-- Le javascript
+	    ================================================== -->
+	    <!-- Placed at the end of the document so the pages load faster -->
+
+		<?php echo $html->script(array(
+			'jquery.min', 
+			'bootstrap.min', 
+			'modules/prompt.modal.js'
+		)); ?>
