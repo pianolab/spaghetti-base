6ec5cdca5c81c239d5bb24fcc6a2b524eff22ec9
Djalma Araújo de Andrade <djalma.araujo@gmail.com>
2012-03-16 15:14:35 -0300

    Some styles

--- app/views/home/index.htm.php
+++ app/views/home/index.htm.php
@@ -2,7 +2,7 @@
 <div class="hero-unit">
   <h1>piano.base</h1>
   <p>SpaghettiPHP framework + Bootstrap, from Twitter (v2.0.2)</p>
-  <p><a href="https://github.com/pianolab/spaghetti-base/zipball/master" class="btn btn-primary btn-large">piano.base download last version</a></p>
+  <p><a href="https://github.com/pianolab/spaghetti-base" class="btn btn-inverse btn-large">piano.base source code</a></p>
 </div>
 
 <!-- Example row of columns -->
@@ -11,7 +11,7 @@
 		<h2>Instructions</h2>
 		<pre>
 1. git clone https://pianolab@github.com/pianolab/spaghetti-base.git
-2. remove folder .git
+2. remove .git folder
 3. work!</pre>
   </div>
 </div>
