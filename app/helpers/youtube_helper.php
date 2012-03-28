fbd86699cf4f0934e6bf4a8f5ab4d91d9b830ffa
Walmir Neto <wfsneto@gmail.com>
2012-03-28 12:07:14 -0300

    Ajustes no helper do youtube e adição do elemento youtube

--- app/helpers/youtube_helper.php
+++ app/helpers/youtube_helper.php
@@ -5,56 +5,72 @@
  *  @license http://www.opensource.org/licenses/mit-license.php
  *  @copyright Copyright 2008-2009, Spaghetti* Framework http://spaghettiphp.org/
  *
+ * Usage on views
+ *  <?php 
+ *  echo $this->element('shared/youtube', array(
+ *    'width' => '970', 
+ *    'height' => '475', 
+ *    'url_video' => $youtube->getUrl($url_video)
+ *  )); 
+ *  ?>
+ *
  */
 class YoutubeHelper extends Helper {
 
-  function thumb($url){
-      $arr = explode("watch?v=",$url);
-                      
-      if(count($arr) == 2) {
-        $resp = explode("&", $arr[1]);
-        if(count($resp) == 0) {
-          $link = $arr[1];
-        } else {
-          $link = $resp[0];
-        } 
-      } else {
-        $arr = explode("/v/", $url);
-        $resp = explode("&", $arr[1]);
-        if(count($resp) == 0) {
-          $link = $arr[1];
-        } else {
-          $link = $resp[0];
-        }
-      }
-      
-      return "http://img.youtube.com/vi/" . $link . "/3.jpg";
+  function thumb($url)
+  {
+    return "http://img.youtube.com/vi/" .$this->returnId($url) . "/3.jpg";
   }
   
-  function getId($url){
-      $arr = explode("watch?v=",$url);
-      //$resp = explode("&", $arr[1]);
-                      
-      if(count($arr) == 2) {  
-        $resp = explode("&", $arr[1]);
-        if(count($resp) == 2) {
-          $link = $arr[1];
-        } else {
-          $link = $resp[0];
-        } 
-      } else {
-        $arr = explode("/v/", $url);
-        $resp = explode("&", $arr[1]);
-        if(count($resp) == 0) {
-          $link = $arr[1];
-        } else {
-          $link = $resp[0];
-        }
-      }
-      
-      return $link;
+  function getId($url)
+  {
+    return $this->returnId($url);
+  }
+
+  function getUrl($url)
+  {
+    return 'http://www.youtube.com/v/' . $this->returnId($url) . '?version=3&amp;hl=pt_BR';
+  }
+
+  private function returnId($url)
+  {
+    $arr = explode("?",$url);
+    $strings = explode("&",$arr[1]);
+    $return = false;
+    foreach ($strings as $key => $string) {
+      $aux = explode('=', $string);
+      $return[$aux[0]] = $aux[1];
+    } # endforeach;
+    $return = $return['v'];
+
+    if ($return) {
+      return $return;
+    } # endif
+    else {
+      $return = false;
+      $arr = explode("/youtu.be/",$url);
+      $aux = explode("/",$arr[1]);
+      $return = $aux[0];
+    } # endelse;
+
+    if ($return) {
+      return $return;
+    } # endif
+    else {
+      $return = false;
+      $arr = explode("/www.youtube.com/", $url);
+      $strings = explode("/",$arr[1]);
+      foreach ($strings as $key => $string) {
+        echo "<br>$key => $string";
+
+        if ($string == 'v') {
+          $return = $strings[$key + 1];
+          break;
+        } # endif;
+      } # endforeach;
+    } # endelse;
+
+    return $return;
   }
 
 }
-
-?>
