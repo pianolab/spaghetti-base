8e82cedd67d7551334136c6dda24d37b2eece116
Walmir Neto <wfsneto@gmail.com>
2012-08-27 15:38:47 -0300

    ajustes no controller de contato

--- app/controllers/contact_controller.php
+++ app/controllers/contact_controller.php
@@ -1,7 +1,8 @@
 <?php
+
 require_once 'lib/utils/Mailer.php';
+  
 class ContactController extends AppController {
-  public $uses = array();
   
   /**
    * Método de envio de e-mail padrão
@@ -13,30 +14,26 @@ class ContactController extends AppController {
 
     if(!empty($this->data)) {
       if ($this->Contact->Validate($this->data)) {
-      
         $mailer = new Mailer(array(
           'from' => array($this->data['email'] => $this->data['name']),
           'to' => Config::read('Mailer.send.default'),
-          'subject' => 'Formulário de Contato [' . Config::read('app.name') . ']',
+          'subject' => 'Formulário de Contato [ ' . Config::read('app.name') . ' ]',
           'views' => array(
             'text/plain' => 'contact/mail_contact.txt',
             'text/html' => 'contact/mail_contact.htm'
           ),
           'layout' => 'mail',
-          'data' => array(
-            'data' => $this->data
-          )
+          'data' => array('data' => $this->data)
         ));
 
         if ($mailer->send()) {
-          Session::writeFlash('site.alert', array('warning', 'Sua mensagem foi enviada com sucesso. <br /> Entreremos em contato com você o mais breve possível. Obrigado.'));
+          Session::writeFlash('site.alert', array('success', 'Sua mensagem foi enviada com sucesso. <br /> Entreremos em contato com você o mais breve possível. Obrigado.'));
           $this->redirect('/');
         } else {
           Session::writeFlash('site.alert', array('warning', 'Preencha os campos corretamente.'));
           Session::writeFlash('form.data', $this->data);
           $this->redirect('/fale-conosco');
         }
-        
       } else {
         Session::writeFlash('site.alert', array('error', $this->Contact->errors));
         Session::writeFlash('form.data', $this->data);
