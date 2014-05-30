6bce2d42d0fa2f9a1f562c859432de2d977bc5e7
Walmir Neto <walmir@pianolab.com.br>
2014-05-30 11:22:51 -0300

    fix class grid

--- app/views/layout/_footer.htm.php
+++ app/views/layout/_footer.htm.php
@@ -1,7 +1,7 @@
 <footer class="container">
   <hr />
   <div class="row">
-    <div class="span12">
+    <div class="col-md-12">
       <p class="pull-left">piano.base <a href="http://www.pianolab.com.br/">@pianolabweb</a>. It"s released under the MIT license.</p>
       <p class="pull-right"><?php echo $html->image("/pianolab.png") ?></p>
     </div>
